cc308a7ae4a85f148bbceda57bc6dbf59cfe81b7
Jassi9927 <123195722+Jassi9927@users.noreply.github.com>
2024-02-18 20:04:16 -0500

    Update AddressModel.cs

--- Handy-Dandy/Models/AddressModel.cs
+++ Handy-Dandy/Models/AddressModel.cs
@@ -6,6 +6,11 @@ namespace Handy_Dandy.Models
 		public AddressModel()
 		{
 		}
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
 	}
 }
 
