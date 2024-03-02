8001f3848c2e933a79eee43ac18e3299a6d28c7f
Catherine <zelda.df95@gmail.com>
2024-03-02 18:31:31 -0500

    This code is part of the data management that uses Firebase as a database. When you click the "Browse" buttons to select JSON files, a connection is established to the Firebase database and the data is loaded into the corresponding collections. Finally, a success or error message is displayed depending on the result of the operation.

--- DataManagement/MainPage.xaml.cs
+++ DataManagement/MainPage.xaml.cs
@@ -7,14 +7,39 @@ namespace DataManagement;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        this.BindingContext = viewModel;
+    }
 
-    async void UploadButton_Clicked(object sender, EventArgs e)
+    async void CategoriesBrowseButton_Clicked(object sender, EventArgs e)
+    {
+        var file = await FilePicker.PickAsync(new PickOptions
+        {
+            PickerTitle = "Select a JSON file"
+        });
+    }
+
+    async void WorkersBrowseButton_Clicked(object sender, EventArgs e)
+    {
+        var file = await FilePicker.PickAsync(new PickOptions
+        {
+            PickerTitle = "Select a JSON file"
+        });
+    }
+
+    async void ServiceWorkersBrowseButton_Clicked(object sender, EventArgs e)
+    {
+        var file = await FilePicker.PickAsync(new PickOptions
+        {
+            PickerTitle = "Select a JSON file"
+        });
+    }
+
+    async void ConfirmButton_Clicked(object sender, EventArgs e)
     {
         try
         {
@@ -31,15 +56,24 @@ public partial class MainPage : ContentPage
                     {
                         string jsonData = await reader.ReadToEndAsync();
 
-                        AppData categories = JsonConvert.DeserializeObject<AppData>(jsonData);
-              
+                        AppData appData = JsonConvert.DeserializeObject<AppData>(jsonData);
+
                         var firebaseClient = new FirebaseClient("https://handy-dandy-1ce26-default-rtdb.firebaseio.com/");
 
-                        foreach (var category in categories.Categories)
-                        {
-                            await firebaseClient.Child("categories").Child(category.CategoryId).PutAsync(category);
-                        }
+                        //foreach (var category in appData.Categories)
+                        //{
+                        //    await firebaseClient.Child("categories").Child(category.CategoryId).PutAsync(category);
+                        //}
 
+                        //foreach (var worker in appData.Workers)
+                        //{
+                        //    await firebaseClient.Child("workers").Child(worker.WorkerID).PutAsync(worker);
+                        //}
+
+                        foreach (var serviceWorker in appData.ServiceWorkers)
+                        {
+                            await firebaseClient.Child("service_workers").Child(serviceWorker.ServiceId).PutAsync(serviceWorker);
+                        }
                         await DisplayAlert("Success", "Data successfully uploaded to Firebase!", "OK");
                     }
                 }
@@ -51,5 +85,4 @@ public partial class MainPage : ContentPage
         }
     }
 }
-
 
