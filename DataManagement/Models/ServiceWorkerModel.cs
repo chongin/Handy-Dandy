119e171a19b36a0e5c8c9935caf2799d4f5b5229
Catherine <zelda.df95@gmail.com>
2024-03-02 18:52:37 -0500

    Create the mode worker models

---
+++ DataManagement/Models/ServiceWorkerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DataManagement.Models
+{
+    public class ServiceWorkerModel
+    {
+        [JsonProperty("service_id")]
+        public string ServiceId { get; set; }
+
+        [JsonProperty("service_name")]
+        public string ServiceName { get; set; }
+
+        [JsonProperty("worker_ids")]
+        public List<string> WorkerIds { get; set; }
+    }
+}
---
+++ DataManagement/Models/WorkerModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DataManagement.Models
+{
+    public class WorkerModel
+    {
+        [JsonProperty("worker_id")]
+        public string WorkerID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("score")]
+        public float Score { get; set; }
+
+        [JsonProperty("ratings")]
+        public int Ratings { get; set; }
+
+        [JsonProperty("labor_cost")]
+        public int LaborCost { get; set; }
+
+        [JsonProperty("image_name")]
+        public string ImageName { get; set; }
+
+        [JsonProperty("role_id")]
+        public int RoleID { get; set; }
+    }
+}
+
