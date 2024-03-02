4306bd518d5e32ccb115e9e85bc9afeda1c256a7
Catherine <zelda.df95@gmail.com>
2024-03-02 18:49:23 -0500

    It contains three properties: Categories, Workers and ServiceWorkers. These properties have the JsonProperty attribute of Newtonsoft.Json, which indicates how the property names should be mapped when serializing and deserializing JSON objects.

--- DataManagement/Models/AppData.cs
+++ DataManagement/Models/AppData.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using Newtonsoft.Json;
 
+using System;
+using Newtonsoft.Json;
+
 namespace DataManagement.Models
 {
-	public class AppData
-	{
+    public class AppData
+    {
         [JsonProperty("categories")]
         public List<CategoryModel> Categories { get; set; }
+
+        [JsonProperty("workers")]
+        public List<WorkerModel> Workers { get; set; }
+
+        [JsonProperty("service_workers")]
+        public List<ServiceWorkerModel> ServiceWorkers { get; set; }
     }
 }
+
 
