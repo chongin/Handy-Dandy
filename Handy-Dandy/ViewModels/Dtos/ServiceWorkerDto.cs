64aa845feccd75b7ed458fcccf45cc4f98b06049
javieracevedo <javier.acevedo233@gmail.com>
2024-03-04 11:33:21 -0500

    Create service worker DTO

---
+++ Handy-Dandy/ViewModels/Dtos/ServiceWorkerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Handy_Dandy.Models;
+using Newtonsoft.Json;
+
+namespace Handy_Dandy.ViewModels.Dtos
+{
+	public class ServiceWorkerDto : ObservableObject
+    {
+        public string ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public List<string> WorkerIds { get; set; }
+
+        public ServiceWorkerDto(ServiceWorkerModel serviceWorkerModel)
+		{
+            ServiceId = serviceWorkerModel.ServiceId;
+            ServiceName = serviceWorkerModel.ServiceName;
+            WorkerIds = new List<string>();
+            WorkerIds = serviceWorkerModel.WorkerIds;
+        }
+    }
+}
+
