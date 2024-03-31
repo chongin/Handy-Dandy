f33f82be6f2b74a541a0389418b98736c4bf8ed4
Catherine <zelda.df95@gmail.com>
2024-03-31 17:19:15 -0400

    This code provides methods to interact with a database in an application with the functionalities to initialize data and manage and also asynchronous methods to perform data insert, update and query operations.

--- Handy-Dandy/Services/IDatabaseService.cs
+++ Handy-Dandy/Services/IDatabaseService.cs
@@ -2,24 +2,25 @@
 using Handy_Dandy.Models;
 namespace Handy_Dandy.Services
 {
-	public interface IDatabaseService
-	{
-		Task InserUser(UserModel user);
-		Task UpdateUser(UserModel user);
-		Task<UserModel> QueryUserByEmail(string email);
+    public interface IDatabaseService
+    {
+        Task InserUser(UserModel user);
+        Task UpdateUser(UserModel user);
+        Task<UserModel> QueryUserByEmail(string email);
         List<CategoryModel> GetCategories();
         List<PromotionModel> GetPromotions();
 
-		Task<List<BookingModel>> GetBookingsByState(string state);
-		Task<WorkerModel> GetWorkerByID(string workerID);
-		Task<ServiceModel> GetServiceByID(string serviceID);
-		Task<List<WorkerModel>> GetWorkersByServiceID(string serviceID);
-		Task<UserModel> GetUserById(string userId);
+        Task<List<BookingModel>> GetBookingsByState(string state);
+        Task<WorkerModel> GetWorkerByID(string workerID);
+        Task<ServiceModel> GetServiceByID(string serviceID);
+        Task<List<WorkerModel>> GetWorkersByServiceID(string serviceID);
+        Task<UserModel> GetUserById(string userId);
     }
 
     public interface IDatabaseService1
     {
         Task InserUser(UserModel user);
+
         Task UpdateUser(UserModel user);
         Task<UserModel> QueryUserByEmail(string email);
         List<PromotionModel> GetPromotions();
@@ -30,8 +31,8 @@ namespace Handy_Dandy.Services
         List<WorkerModel> GetWorkersByServiceId(string serviceId);
         List<WorkerModel> GetWorkerModels();
 
-        Task InitData(); 
-        
+        Task InitData();
+        Task InserBooking(string userName, BookingModel model);
     }
 }
 
